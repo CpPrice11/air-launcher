8333cee9b28634240a432fde09e9ec3c3025fde5
Air Launcher Developer <dev@airlauncher.local>
2026-05-17 15:22:33 +0300

    Release v1.1.1

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -9,7 +9,7 @@ import './PageStyles.css'
 
 const LAUNCHER_OWNER = 'CpPrice11'
 const LAUNCHER_REPO = 'air-launcher'
-const CURRENT_VERSION = 'v1.1.0'
+const CURRENT_VERSION = 'v1.1.1'
 
 type PendingLauncherAction = {
   release: GitHubRelease
