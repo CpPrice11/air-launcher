9db94c6c0cac33d1a4a097eccbce4fa9fdc5f15f
Air Launcher Developer <dev@airlauncher.local>
2026-06-10 22:17:55 +0300

    Make library more Steam-like

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -17,7 +17,7 @@ import './PageStyles.css'
 
 const LAUNCHER_OWNER = 'CpPrice11'
 const LAUNCHER_REPO = 'air-launcher'
-const FALLBACK_CURRENT_VERSION = 'v5.0.2'
+const FALLBACK_CURRENT_VERSION = 'v5.0.3'
 
 type PendingLauncherAction = {
   release: GitHubRelease
